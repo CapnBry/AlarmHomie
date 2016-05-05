e45f86b1ae31ad7b8dc82df80b0fcdda32d38c84
Bryan Mayland <bmayland@capnbry.net>
2016-05-05 10:37:34 -0400

    Remove DHT measuring stuff

--- src/AlarmHomie.ino
+++ src/AlarmHomie.ino
@@ -1,8 +1,6 @@
 #include <vector>
-#include <DHT.h>
 #include <Homie.h>
 
-//#define DPIN_DHT   2        // D4 on NodeMCU (already pulled up 12k)
 #define DPIN_ZONE1 4		// D2 on NodeMCU
 #define DPIN_ZONE2 5		// D1 on NodeMCU
 #define DPIN_ZONE3 12       // D6 on NodeMCU
@@ -12,10 +10,6 @@
 #define RETRIGGER_INTERVAL 10000U // how often a state change can occur (ms)
 
 HomieNode alarmNode("alarm", "multizone-alarm");
-#if defined(DPIN_DHT)
-HomieNode dhtNode("dht22", "environmental");
-DHT dht(DPIN_DHT, DHT22);
-#endif // DPIN_DHT
 
 class AlarmPin
 {
@@ -131,85 +125,25 @@ bool checkAlarm()
   return false;
 }
 
-bool checkTemp()
-{
-#if defined(DPIN_DHT)
-  const uint32_t UPDATE_INTERVAL = 60000U;
-  static uint32_t lastUpdate;
-  if (millis() - lastUpdate > UPDATE_INTERVAL)
-  {
-    float humidity;
-    humidity = dht.readHumidity();
-
-    if (!isnan(humidity))
-    {
-      // Read temperature before publishing to be sure to not force another read
-      float temp = dht.readTemperature();
-      Homie.setNodeProperty(dhtNode, "humidity", String(humidity, 1), false);
-
-      if (!isnan(temp))
-      {
-        Homie.setNodeProperty(dhtNode, "temperature", String(temp, 1), false);
-      }
-    }
-    else
-      Homie.setNodeProperty(dhtNode, "error", "nan", false);
-
-    lastUpdate = millis();
-    return true;
-  }
-#endif // DPIN_DHT
-  return false;
-}
-
-#if defined(DPIN_DHT)
-bool dhtDhtCallback(String message)
-{ 
-  return true;
-
-  const uint8_t _pin = DPIN_DHT;
-  //digitalWrite(_pin, HIGH);
-  //delay(250);
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, LOW);
-  delay(20);
-
-  pinMode(_pin, INPUT_PULLUP);
-  for (uint16_t cnt = 0; cnt < 50000; ++cnt)
-  {
-    if (digitalRead(_pin) == LOW)
-    {
-      Serial.print("Line went low: "); Serial.println(cnt, DEC); 
-      return true;
-    }
-  }
-  Serial.println("Line did not go low");
-  return true;
-}
-#endif // DHT
 
 void setupHandler()
 {
   digitalWrite(DPIN_COMP_REF, LOW);
   pinMode(DPIN_COMP_REF, OUTPUT);
 
-  //pins.push_back(new AlarmPin(0, "flash"));
   pins.push_back(new AlarmPin(DPIN_ZONE1, "zone1"));
   pins.push_back(new AlarmPin(DPIN_ZONE2, "zone2"));
   pins.push_back(new AlarmPin(DPIN_ZONE3, "zone3"));
   pins.push_back(new AlarmPin(DPIN_ZONE4, "zone4"));
-
-#if defined(DPIN_DHT)
-  dht.begin();
-#endif // DHT
 }
 
 void loopHandler()
 {
-  checkAlarm() || checkTemp();
+  checkAlarm();
 }
 
 void setup() {
+  // Blink the LED a while to allow the serial port time to connect
   pinMode(BUILTIN_LED, OUTPUT);
   for (unsigned char i=0; i<25; ++i)
   {
@@ -223,10 +157,6 @@ void setup() {
   Homie.setSetupFunction(setupHandler);
   Homie.setLoopFunction(loopHandler);
   Homie.registerNode(alarmNode);
-#if defined(DPIN_DHT)
-  dhtNode.subscribe("measure", dhtDhtCallback);
-  Homie.registerNode(dhtNode);
-#endif
   Homie.setup();
 }
 
